7fea4137562893819132a819c29f27722b43484e
Anshul <discover.letsfindit@gmail.com>
2022-05-27 19:11:25 +0530

    v 0.1 By Anshul

---
+++ includes/DBcon.php
@@ -0,0 +1,32 @@
+<?php
+
+    class DBcon
+    {
+        private string $servername;
+        private string $username;
+        private string $password;
+        private string $dbname;
+        private bool $betaMode;
+
+        public function __construct()
+        {
+            $this->servername = "localhost";
+            $this->username = "root";
+            $this->password = "";
+            $this->dbname = "adrexDB";
+            $this->betaMode = false;
+        }
+
+        public function connect(): void
+        {
+            try {
+                $conn = new PDO("mysql:host=$this->servername; $this->dbname", $this->username, $this->password);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                if ($this->betaMode)
+                    echo "Connected successfully";
+            } catch (PDOException $e) {
+                if ($this->betaMode)
+                    echo "Connection failed: " . $e->getMessage();
+            }
+        }
+    }
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    require_once('includes/DBcon.php');
+    
+    $connection = new DBcon();
+    $connection->connect();
+
+
+?>
+<html lang="en">
+<title>
+    Adrex
+</title>
+<body>
+<h1>Hello</h1>
+<?php
+
+?>
+</body>
+</html>
